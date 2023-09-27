5505701958c2635d789f00d1b8f2b22146118cf9
alahirajeffrey <alahirajeffrey@gmail.com>
2023-09-27 23:05:45 +0100

    switch case in glo

---
+++ 04-switch.go
@@ -0,0 +1,16 @@
+package main
+
+func fourthMain() {
+	animal := "dog"
+
+	switch animal {
+	case "cat":
+		println("meow")
+	case "dog":
+		println("woof")
+	case "frog":
+		println("ribbit")
+	case "hyena":
+		println("hihihi")
+	}
+}
